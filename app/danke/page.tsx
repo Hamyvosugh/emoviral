b759ab579fa8602383394debca76675a65f3ded3
hamyvosugh <hamy.vosugh@gmail.com>
2025-02-18 00:45:18 +0100

    qwe

--- app/danke/page.tsx
+++ app/danke/page.tsx
@@ -1,15 +1,14 @@
 'use client';
+import { useSearchParams } from 'next/navigation';
 import ThankYouPage from '@/components/Form/ThankYouPage';
 import Layout from '@/components/Layout/Layout';
 
-export default function DankePage({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
+export default function DankePage() {
+  const searchParams = useSearchParams();
+
   return (
     <Layout>
-      <ThankYouPage searchParams={searchParams} />
+      <ThankYouPage searchParams={Object.fromEntries(searchParams.entries())} />
     </Layout>
   );
 }
